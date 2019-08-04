4c25f63fd87f8f1ce0167127ba4210a402b2d3cb
Gaurav <cQBzuy/JMRQ9FfDiicon8xnubU5K7Uy6+PDzys0P7V0=>
2019-08-04 16:08:13 +0530

    Android JetPack components Usage

---
+++ app/src/main/java/com/gaurav/jetpacknavigationcomponent/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.gaurav.jetpacknavigationcomponent
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.content_main)
+        setSupportActionBar(toolbar)
+    }
+}
